7987574073307267f5a86712c5dd665f7948c997
Zain <m.zainhassanbaloch@gmail.com>
2024-08-04 15:24:53 +0500

    logo bug fixed

--- components/shared/Navbar/NavLogo.tsx
+++ components/shared/Navbar/NavLogo.tsx
@@ -3,34 +3,38 @@
 import { useToggleContext } from "@/providers/ToggleProvider";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
-const NavLogo = ({lng} : {lng: string;}) => {
-    const router = useRouter();
+const NavLogo = ({ lng }: { lng: string }) => {
     const { setNavUserToggle } = useToggleContext();
     const pathname = usePathname();
-    const isDashboard = pathname.includes('/dashboard')
+    const isDashboard = pathname.includes('/dashboard');
+
     return (
         <Link
             href={`/${lng}`}
             onClick={() => setNavUserToggle(false)}
-            className="h-[24px] md:h-[48px] md:w-[336px] w-[200px] relative">
-            {!isDashboard ? <Image
-                className="cursor-pointer"
-                src="/images/navLogo.svg"
-                alt="Logo"
-                height={24}
-                width={336}
-            /> : <Image
-                onClick={() => router.push(`/${lng}`)}
-                className="cursor-pointer"
-                src="/images/navlogo_transparent.png"
-                alt="Logo"
-                height={24}
-                width={336}
-            />}
+            className="h-[24px] md:h-[48px] md:w-[336px] w-[200px] relative"
+        >
+            {!isDashboard ? (
+                <Image
+                    className="cursor-pointer"
+                    src="/images/navLogo.svg"
+                    alt="Logo"
+                    height={24}
+                    width={336}
+                />
+            ) : (
+                <Image
+                    className="cursor-pointer"
+                    src="/images/navlogo_transparent.png"
+                    alt="Logo"
+                    height={24}
+                    width={336}
+                />
+            )}
         </Link>
     );
-}
+};
 
 export default NavLogo;
